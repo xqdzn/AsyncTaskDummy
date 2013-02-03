acec4e788d9adc7720f15060e3920d506c05fe00
Mizan Ridho <exqdzn@gmail.com>
2013-02-03 18:30:32 +0700

    Gotcha!

--- src/com/anansi/interactive/eg/asynctaskdummy/Home.java
+++ src/com/anansi/interactive/eg/asynctaskdummy/Home.java
@@ -1,38 +1,91 @@
 package com.anansi.interactive.eg.asynctaskdummy;
 
+import java.net.URL;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.Menu;
+import android.os.StrictMode;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class Home extends Activity {
-	Button fetchBtn;
+	Button tombol;
 	EditText url;
+	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		//getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.home_act);
-	
-		fetchBtn = (Button)findViewById(R.id.button1);
-		url		 = (EditText)findViewById(R.id.editText1);
-		
-		//fetchFileTask().execute("http://i47.tinypic.com/2a7ei5w.gif");
-		
-		
-	}
-	
-	private class fetchFileTask() extends AsyncTask<URL, Integer, Long>{
-		
-	}
-	
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.home_act, menu);
-		return true;
-	}
 
+		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+		StrictMode.setThreadPolicy(policy);
+
+		url	= (EditText) findViewById(R.id.editText1);
+		tombol	= (Button)	 findViewById(R.id.button1);
+
+		//final String _url = url.getText().toString();
+		
+		
+		tombol.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				CobakAja cobakk = new CobakAja();
+				cobakk.execute(url.getText().toString());
+			}
+		});
+		
+		
+	}
+			
+			
+	
+	
+	public  class CobakAja extends AsyncTask<String, Void, String> {
+		protected void onPreExecute(){
+			Toast.makeText(Home.this, "preExecute", Toast.LENGTH_LONG).show();
+		}
+		@Override
+		protected String doInBackground(String... params) {
+			// TODO Auto-generated method stub
+			String lele = params[0];
+			Log.e("begron", "bernilai " + lele);
+	        return lele;
+		}
+				 
+		 
+		 protected void onPostExecute(String result){
+		       
+			 	Log.e("PostExecuteee", "AsyncTask returned : " + result);
+		        alertbox(result);
+		    }
+
+		
+		}
+		
+	
+		
+		
+	
+	public void alertbox(String pesan)
+	{   new AlertDialog.Builder(this)
+	        .setMessage(pesan)
+	        .setNeutralButton(android.R.string.cancel,
+	                new DialogInterface.OnClickListener() 
+	                {   public void onClick(DialogInterface dialog, int whichButton) {}
+	                })
+	        .show();
+	}
 }
